b3c1ac75de66292686f66437ac80a0e3ba3c9dbe
mart-anthony-stark <martsalazar0@gmail.com>
2023-05-14 16:03:14 +0800

    add set loading false

--- src/hooks/useResetPass.js
+++ src/hooks/useResetPass.js
@@ -17,12 +17,14 @@ export default function useResetPass() {
           `${process.env.REACT_APP_API_URL}/resetcode?email=${email}`
         );
         const json = await res.json();
+        setLoading(false);
         if (!res.ok) {
           toast.error(json.msg);
         } else {
           toast.success("Email was successfully send to your account!");
         }
       } catch (error) {
+        setLoading(false);
         console.log(error);
       }
     }
