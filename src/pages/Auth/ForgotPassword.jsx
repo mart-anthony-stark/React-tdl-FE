313d856752fbcda6c3012ddab761d844595bf6da
mart-anthony-stark <martsalazar0@gmail.com>
2023-05-14 16:03:25 +0800

    Add loader

--- src/pages/Auth/ForgotPassword.jsx
+++ src/pages/Auth/ForgotPassword.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "./auth.css";
 import useResetPass from "../../hooks/useResetPass";
 import { useState } from "react";
+import FetchLoading from "../../components/FetchLoading/FetchLoading";
 
 export default function ForgotPassword() {
   const { isLoading, sendResetCode } = useResetPass();
@@ -13,6 +14,7 @@ export default function ForgotPassword() {
   };
   return (
     <div className="forgot_password center col">
+      {isLoading ? <FetchLoading /> : null}
       <h1>Forgot Password</h1>
       <div>
         Enter the email address associated with your
