625bc04e5eebf761e2bdfee21bd569df99c14d9a
mart-anthony-stark <martsalazar0@gmail.com>
2023-05-14 15:57:54 +0800

    add reset password

---
+++ src/hooks/useResetPass.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+export default function useResetPass() {
+  const [isLoading, setLoading] = useState();
+
+  const sendResetCode = async (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+
+    if (!isValidEmail) {
+      toast.error("Invalid email.");
+    } else {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `${process.env.REACT_APP_API_URL}/resetcode?email=${email}`
+        );
+        const json = await res.json();
+        if (!res.ok) {
+          toast.error(json.msg);
+        } else {
+          toast.success("Email was successfully send to your account!");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  return {
+    isLoading,
+    sendResetCode,
+  };
+}
